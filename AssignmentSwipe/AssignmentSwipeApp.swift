62a77a531e10d971016586e874e343d597a199b5
aarish21 <aarish.jerry@gmail.com>
2024-06-09 14:14:35 +0530

    Initial Commit

---
+++ AssignmentSwipe/AssignmentSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentSwipeApp.swift
+//  AssignmentSwipe
+//
+//  Created by Aarish on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentSwipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssignmentSwipe
+//
+//  Created by Aarish on 09/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
